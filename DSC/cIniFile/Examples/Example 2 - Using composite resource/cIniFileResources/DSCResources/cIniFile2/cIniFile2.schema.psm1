5fbb3817096ed894623bdd97b60f243a571537cc
John Roos <john.m.roos@gmail.com>
2016-03-28 22:31:50 +0200

    Updated examples  - Moved examples into subfolders  - Added example with composite resource for those who like to make it more strict instead of using hash table for ini config

---
+++ DSC/cIniFile/Examples/Example 2 - Using composite resource/cIniFileResources/DSCResources/cIniFile2/cIniFile2.schema.psm1
@@ -0,0 +1,31 @@
+﻿Configuration cIniFile2
+{
+    param
+    (
+        # Path to ini file
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String] $Path,
+
+        # Debug mode (true or false)
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateSet("true", "false")]
+        [String] $EnableDebug,
+
+        # Logfile path
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [String] $LogPath
+    )
+
+    Import-DscResource -ModuleName cIniFile
+    cIniFile MyFile
+    {
+        Path   = $Path
+        Config = @{
+            '[Config]Debug'   = $EnableDebug
+            '[Config]Logpath' = $LogPath
+        }
+    }
+}
---
+++ DSC/cIniFile/Examples/Example 2 - Using composite resource/cIniFile_config_example.ps1
@@ -0,0 +1,14 @@
+﻿Configuration MyIniFile
+{
+    Import-DscResource -ModuleName cIniFileResources
+ 
+    cIniFile2 MyFile
+    {
+        Path        = 'c:\temp\inifile.ini'
+        EnableDebug = 'true'
+        LogPath     = 'C:\logfile.log'
+    }
+}
+
+# MyIniFile -OutputPath c:\temp\MyIniFile\
+# Start-DscConfiguration -Path C:\temp\MyIniFile\ -Wait -Verbose -Force
